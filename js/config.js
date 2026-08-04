20414fa64b73053fa51889bc2702bd0e9ba7d6f3
avnergomes <avnerpaesgomes@gmail.com>
2026-08-04 13:46:56 -0300

    feat: ativa WhatsApp 5541988636987 como canal de pedidos

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@ window.D3D_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbz_xwtIVFqFNrI10jAg7F7t9GkPoyOlyrzumLMhT8yA6mC56NLh2HgGFy-lSJL8JGip/exec',
 
   // WhatsApp comercial no formato 55DDDNUMERO (ex: 5541999998888).
-  // Enquanto vazio, o pedido é enviado por e-mail.
-  WHATSAPP: '',
+  // Se vazio, o pedido cai para e-mail.
+  WHATSAPP: '5541988636987',
 
   EMAIL: 'd3dinovacao@gmail.com',
   INSTAGRAM: 'https://www.instagram.com/d3dinovacao',
